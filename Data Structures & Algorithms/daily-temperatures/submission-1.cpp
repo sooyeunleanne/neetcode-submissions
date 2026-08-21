7580bf7f2f7e9d6fd736b846465e07659c4a3327
Leanne Kim <sooyeunleanne@gmail.com>
2026-08-21 16:36:31 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        stack<int> st;
+
+        for (int i = 0; i < temperatures.size(); i++) {
+            while (!st.empty() && (temperatures[i] > temperatures[st.top()])) {
+                int prevDay = st.top();
+                st.pop();
+
+                result[prevDay] = i - prevDay;
+            }
+
+            st.push(i);
+        }
+
+        return result;
+    }
+};
